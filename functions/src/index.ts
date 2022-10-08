5a693b5a34c9acb43ec045a22e88243820d8a565
Imon Roy <royimonroy@gmail.com>
2022-10-08 15:28:45 +0530

    Updated Commands

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,6 +17,26 @@ bot.on('message', (ctx: {
     chat: { id: any; }; message: any;
 }) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message))
 
+bot.command('/hacktoberfest', (ctx: { reply: (arg0: string) => any; }) =>
+    ctx.reply(`Hello Guys,
+    Hacktoberfest is staring from tonight 12am🥳
+    
+    Wanna get amazing swags right!🌚
+    We are hosting 2 repos these time from GDSC-NSEC, For more info, go thorugh the readme section of the repo🤞🏻
+    All these are begineer friendly repo's. From freshman to final year, anyone can contruibute🫂💓
+    
+    How to contruibute?
+    
+    - Create an issue if you want to add any feature or you get any bug! (You can ask here also, if you are stucked up)
+    - We will mark the hacktoberfest label!
+    - Start working
+    - Make the Pull Request.
+    - We will merge it in our repo, after reviewing the code!
+    
+    Create 4 Pull request, and it should not be duplicate (Be careful). 
+    Congratulation's, You are eligible for swags claimation!`)
+)
+
 
 exports.echoBot = functions.https.onRequest(async (request: { body: any; }, response: { sendStatus: (arg0: number) => any; }) => {
     functions.logger.log('Incoming message', request.body)
